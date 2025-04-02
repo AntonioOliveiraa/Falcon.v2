05ceac0c2900bf841169b84c76ac6cf2d355ac9c
AntonioOliveiraa <antoniooliveiraantonio-augusto@outlook.com>
2025-04-01 22:31:49 -0300

    Adiciona funções de interrupção para contagem de RPM dos motores

--- src/main.cpp
+++ src/main.cpp
@@ -38,104 +38,7 @@ Adafruit_TCS34725 tcs = Adafruit_TCS34725();
 // Initialise with specific int time and gain values 
 // Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_600MS, TCS34725_GAIN_1X);
 
-//
-
-void setup() {
-  // Init Serial
-  Serial.begin(115200);
-
-  // Inicializa o sensor ultrassônico
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-  last_Calc_A = millis();
-
-  if (tcs.begin()) {
-    Serial.println("Found TCS34725 sensor");
-  } else {
-    Serial.println("No TCS34725 found... check your connections");
-    while (1);
-  }
-
-  tcs.setIntegrationTime(TCS34725_INTEGRATIONTIME_300MS);
-  //tcs.setGain(TCS34725_GAIN_1X);
-
-  // Inicializa as interrupcoes com os pinos configurados para chamar as funcoes  
-  attachInterrupt(digitalPinToInterrupt(PIN_A_ENCODER), countA, RISING);
-  attachInterrupt(digitalPinToInterrupt(PIN_B_ENCODER), countB, RISING);
-
-  }
-
-void loop() {
-  motor1->enable();
-  motor2->enable();
-  delay(5000);
-  moveForward(motor1, motor2, 140);
-  delay(5000);
-  moveBackward(motor1, motor2, 140);
-  delay(5000);
-  stopMove(motor1, motor2);
-  delay(1000);
-  Serial.println("Moving Motor 1 forward!");
-  motorRamp(motor1, 130, 190, FORWARDMODE, 30);
-  delay(5000);
-  motorRamp(motor1, 190, 130, FORWARDMODE, 30);
-  delay(5000);
-  Serial.println("Moving Motor 1 backward!");
-  motorRamp(motor1, 130, 190, BACKWARDMODE, 30);
-  delay(5000);
-  motorRamp(motor1, 190, 130, BACKWARDMODE, 30);
-  delay(15000);
-
-  Serial.println("Moving Motor 2 forward!");
-  motorRamp(motor2, 130, 190, FORWARDMODE, 30);
-  delay(3000);
-  motorRamp(motor2, 190, 130, FORWARDMODE, 30);
-  delay(3000);
-  Serial.println("Moving Motor 2 backward!");
-  motorRamp(motor2, 130, 190, BACKWARDMODE, 30);
-  delay(3000);
-  motorRamp(motor2, 190, 130, BACKWARDMODE, 30);
-  
-  uint16_t r, g, b, c, colorTemp, lux;
-
-  tcs.getRawData(&r, &g, &b, &c);
-  colorTemp = tcs.calculateColorTemperature(r, g, b);
-  lux = tcs.calculateLux(r, g, b);
-  
-
-  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC);
-  Serial.print(" K - ");
-  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
-  delay(1000);
-
-  (readUltrasonic() <= 10) ? stopMove(motor1, motor2) : (void)0;
-}
-
-int readUltrasonic(){
-  long duration, cm;
-  // Leitura da distância do sensor ultrassônico
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
-  cm = (duration / 2) / 29.1; // Conversão de microssegundos para centímetros
-
-  // Impressão da distância no monitor serial
-  Serial.print("Distância: ");
-  Serial.print(cm);
-  Serial.println(" cm");
-  return cm;
-}
+bool mover = true;
 
 // Interrupt Function
 void countA() {
@@ -165,4 +68,86 @@ void countB() {
     Serial.print("RPM B: ");
     Serial.println(rpm_B);
   }
+}
+
+void setup() {
+  // Init Serial
+  Serial.begin(115200);
+
+  // Inicializa o sensor ultrassônico
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  last_Calc_A = millis();
+
+  if (tcs.begin()) {
+    Serial.println("Found TCS34725 sensor");
+  } else {
+    Serial.println("No TCS34725 found... check your connections");
+    while (1);
+  }
+
+  tcs.setIntegrationTime(TCS34725_INTEGRATIONTIME_300MS);
+  //tcs.setGain(TCS34725_GAIN_1X);
+
+  // Inicializa as interrupcoes com os pinos configurados para chamar as funcoes  
+  attachInterrupt(digitalPinToInterrupt(PIN_A_ENCODER), countA, RISING);
+  attachInterrupt(digitalPinToInterrupt(PIN_B_ENCODER), countB, RISING);
+
+}
+
+
+void loop() {
+  motor1->enable();
+  motor2->enable();
+
+  // motorRamp(motor, vel. init, vel. fin., mode, interval-delay);
+  // moveForward(motor1, motor2, velocidade);
+  // moveBackward(motor1, motor2, velocidade);
+  // stopMove(motor1, motor2);
+
+  if (mover) {
+    moveForward(motor1, motor2, 90);
+    delay(2000);
+    mover = false;
+  }
+  
+  // uint16_t r, g, b, c, colorTemp, lux;
+
+  // tcs.getRawData(&r, &g, &b, &c);
+  // colorTemp = tcs.calculateColorTemperature(r, g, b);
+  // lux = tcs.calculateLux(r, g, b);
+  
+
+  // Serial.print("Color Temp: "); Serial.print(colorTemp, DEC);
+  // Serial.print(" K - ");
+  // Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+  // Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+  // Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+  // Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+  // Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+  // Serial.println(" ");
+  // delay(1000);
+
+  // (readUltrasonic() <= 10) ? stopMove(motor1, motor2) : (void)0;
+}
+
+int readUltrasonic(){
+  long duration, cm;
+  // Leitura da distância do sensor ultrassônico
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+  cm = (duration / 2) / 29.1; // Conversão de microssegundos para centímetros
+
+  // Impressão da distância no monitor serial
+  Serial.print("Distância: ");
+  Serial.print(cm);
+  Serial.println(" cm");
+  return cm;
 }
